113432011f32c63ef2ec24b8023c788208aae04f
Naitik Shah <n@daaku.org>
2022-10-21 20:55:59 +0400

    prettier changes for leaner coding style

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,72 @@
 // Storage to persist authentication information.
 export interface AuthStorage {
-  set(key: string, value: string): Promise<void>;
-  get(key: string): Promise<string | undefined>;
-  remove(key: string): Promise<void>;
+  set(key: string, value: string): Promise<void>
+  get(key: string): Promise<string | undefined>
+  remove(key: string): Promise<void>
 }
 
 // The minimal User maintained for Authentication and Authorization purposes.
 export interface User {
-  localId: string;
-  email: string;
-  refreshToken: string;
-  idToken: string;
-  expiresAt: number;
+  localId: string
+  email: string
+  refreshToken: string
+  idToken: string
+  expiresAt: number
 }
 
 // There are other fields that may be included as documented at:
 // https://cloud.google.com/identity-platform/docs/reference/rest/v1/accounts/signUp
 // An empty object will create an anonymous user if the project allows for it.
 export interface SignUpRequest {
-  email?: string;
-  password?: string;
-  displayName?: string;
-  photoUrl?: string;
+  email?: string
+  password?: string
+  displayName?: string
+  photoUrl?: string
 }
 
 // There are other fields that may be included as documented at:
 // https://cloud.google.com/identity-platform/docs/reference/rest/v1/accounts/signInWithPassword
 export interface SignInRequest {
-  email: string;
-  password: string;
+  email: string
+  password: string
 }
 
 // There are other fields that may be included as documented at:
 // https://cloud.google.com/identity-platform/docs/reference/rest/v1/accounts/resetPassword
 export interface ResetPasswordRequest {
-  oobCode?: string;
-  email?: string;
-  oldPassword?: string;
-  newPassword: string;
+  oobCode?: string
+  email?: string
+  oldPassword?: string
+  newPassword: string
 }
 
 const defaultStorage: AuthStorage = {
   async set(k: string, v: string): Promise<void> {
-    localStorage.setItem(k, v);
+    localStorage.setItem(k, v)
   },
   async get(k: string): Promise<string | undefined> {
-    return localStorage.getItem(k) ?? undefined;
+    return localStorage.getItem(k) ?? undefined
   },
   async remove(k: string): Promise<void> {
-    localStorage.removeItem(k);
+    localStorage.removeItem(k)
   },
-};
+}
 
 // Authentication using Firebase API key.
 export class Auth {
-  readonly #apiKey: string;
-  readonly #storage: AuthStorage;
-  readonly #kUser: string;
-  readonly #kEmail: string;
-  #user?: User;
-  #refresh?: Promise<void>;
-  #subscribers: { (user: User | undefined): void }[] = [];
+  readonly #apiKey: string
+  readonly #storage: AuthStorage
+  readonly #kUser: string
+  readonly #kEmail: string
+  #user?: User
+  #refresh?: Promise<void>
+  #subscribers: { (user: User | undefined): void }[] = []
 
   private constructor(apiKey: string, storage: AuthStorage, name: string) {
-    this.#apiKey = apiKey;
-    this.#storage = storage;
-    this.#kUser = `auth:user:${name}:${this.#apiKey}`;
-    this.#kEmail = `auth:email:${name}:${this.#apiKey}`;
+    this.#apiKey = apiKey
+    this.#storage = storage
+    this.#kUser = `auth:user:${name}:${this.#apiKey}`
+    this.#kEmail = `auth:email:${name}:${this.#apiKey}`
   }
 
   // Construct a new Auth instance.
@@ -75,132 +75,132 @@ export class Auth {
     storage = defaultStorage,
     name = '',
   }: {
-    apiKey: string;
-    storage?: AuthStorage;
-    name?: string;
+    apiKey: string
+    storage?: AuthStorage
+    name?: string
   }): Promise<Auth> {
-    const auth = new Auth(apiKey, storage, name);
-    const j = await auth.#storage.get(auth.#kUser);
+    const auth = new Auth(apiKey, storage, name)
+    const j = await auth.#storage.get(auth.#kUser)
     if (j) {
-      await auth.setUser(JSON.parse(j), false);
+      await auth.setUser(JSON.parse(j), false)
     }
-    return auth;
+    return auth
   }
 
   // Get the current User if available.
   public get user(): User | undefined {
-    return this.#user;
+    return this.#user
   }
 
   private async setUser(user?: User, save = true) {
-    const old = this.#user?.localId;
-    this.#user = user;
+    const old = this.#user?.localId
+    this.#user = user
     if (old !== user?.localId) {
-      this.#subscribers.forEach((cb) => cb(this.#user));
+      this.#subscribers.forEach(cb => cb(this.#user))
     }
     if (save) {
       if (user) {
-        await this.#storage.set(this.#kUser, JSON.stringify(this.#user));
+        await this.#storage.set(this.#kUser, JSON.stringify(this.#user))
       } else {
-        await this.#storage.remove(this.#kUser);
+        await this.#storage.remove(this.#kUser)
       }
     }
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private async handleResponse(d: any): Promise<void> {
-    const old = this.#user;
+    const old = this.#user
     const expiresAt =
-      Date.now() + parseInt(d.expiresIn ?? d.expires_in ?? 3600) * 1000;
+      Date.now() + parseInt(d.expiresIn ?? d.expires_in ?? 3600) * 1000
     const user: User = {
       localId: d.localId ?? old?.localId,
       email: d.email ?? old?.email,
       refreshToken: d.refreshToken ?? d.refresh_token ?? old?.refreshToken,
       idToken: d.idToken ?? d.id_token ?? old?.idToken,
       expiresAt,
-    };
-    await this.setUser(user);
+    }
+    await this.setUser(user)
   }
 
   // Sign out the User and clear stored Auth data.
   public async signOut(): Promise<void> {
-    await this.setUser();
+    await this.setUser()
   }
 
   // Sign-Up a new user.
   // https://cloud.google.com/identity-platform/docs/reference/rest/v1/accounts/signUp
   public async signUp(req: SignUpRequest): Promise<void> {
-    const data = await this.api('signUp', req);
-    await this.handleResponse(data);
+    const data = await this.api('signUp', req)
+    await this.handleResponse(data)
   }
 
   // Sign-In exsiting user.
   // https://cloud.google.com/identity-platform/docs/reference/rest/v1/accounts/signInWithPassword
   public async signIn(req: SignInRequest): Promise<void> {
-    const data = await this.api('signInWithPassword', req);
-    await this.handleResponse(data);
+    const data = await this.api('signInWithPassword', req)
+    await this.handleResponse(data)
   }
 
   // Reset password for user.
   // https://cloud.google.com/identity-platform/docs/reference/rest/v1/accounts/resetPassword
   public async resetPassword(req: ResetPasswordRequest): Promise<void> {
-    await this.api('resetPassword', req);
+    await this.api('resetPassword', req)
   }
 
   // Delete the user. The removes the account entirely.
   // https://cloud.google.com/identity-platform/docs/reference/rest/v1/accounts/delete
   public async delete(): Promise<void> {
-    await this.api('delete', { idToken: this.user?.idToken });
-    await this.signOut();
+    await this.api('delete', { idToken: this.user?.idToken })
+    await this.signOut()
   }
 
   // Get the bearer token, if one is available. Refresh it if necessary. This
   // method can be used as a TokenSource in @daaku/firebase-rest-api.
   public async getBearerToken(): Promise<string | undefined> {
     if (!this.#user?.refreshToken) {
-      return;
+      return
     }
-    await this.refresh();
-    return this.#user?.idToken;
+    await this.refresh()
+    return this.#user?.idToken
   }
 
   // Subscribe to get notified of user changes. Callback is invoked once
   // immediately with current user. Returned function can be used to
   // unsubscribe.
   public subscribe(cb: (user: User | undefined) => void): () => void {
-    this.#subscribers.push(cb);
-    cb(this.#user);
+    this.#subscribers.push(cb)
+    cb(this.#user)
     return () => {
-      this.#subscribers = this.#subscribers.filter((e) => e !== cb);
-    };
+      this.#subscribers = this.#subscribers.filter(e => e !== cb)
+    }
   }
 
   // Send a link to the provided email address that allows signing in.
   public async sendEmailSigninLink(email: string): Promise<void> {
-    await this.#storage.set(this.#kEmail, email);
+    await this.#storage.set(this.#kEmail, email)
     await this.api('sendOobCode', {
       requestType: 'EMAIL_SIGNIN',
       email,
-    });
+    })
   }
 
   // This should be invoked on the page where the user lands from an email sign
   // in link.
   public async handleEmailSigninRedirect(url = location.href): Promise<void> {
-    const oobCode = url.match(/[?&]oobCode=([^&]+)/)?.[1];
+    const oobCode = url.match(/[?&]oobCode=([^&]+)/)?.[1]
     if (!oobCode) {
-      throw new Error('oobCode not found in URL');
+      throw new Error('oobCode not found in URL')
     }
-    const email = await this.#storage.get(this.#kEmail);
+    const email = await this.#storage.get(this.#kEmail)
     if (!email) {
-      throw new Error('email not found in storage');
+      throw new Error('email not found in storage')
     }
     const data = await this.api('signInWithEmailLink', {
       oobCode,
       email,
-    });
-    await this.handleResponse(data);
-    await this.#storage.remove(this.#kEmail);
+    })
+    await this.handleResponse(data)
+    await this.#storage.remove(this.#kEmail)
   }
 
   // This allows calling for the various APIs documented here:
@@ -212,36 +212,36 @@ export class Auth {
         ? `https://securetoken.googleapis.com/v1/token?key=${this.#apiKey}`
         : `https://identitytoolkit.googleapis.com/v1/accounts:${endpoint}?key=${
             this.#apiKey
-          }`;
+          }`
     const response = await fetch(url, {
       method: 'post',
       body: JSON.stringify(body),
-    });
-    const data = await response.json();
+    })
+    const data = await response.json()
     if (!response.ok) {
-      throw Error(data);
+      throw Error(data)
     }
-    return data;
+    return data
   }
 
   private async refresh(): Promise<void> {
     if (!this.#user) {
-      throw new Error('refresh called without existing user');
+      throw new Error('refresh called without existing user')
     }
     if (Date.now() < this.#user.expiresAt) {
-      return;
+      return
     }
     if (!this.#refresh) {
-      const refreshToken = this.#user.refreshToken;
+      const refreshToken = this.#user.refreshToken
       this.#refresh = (async () => {
         const data = await this.api('token', {
           grant_type: 'refresh_token',
           refresh_token: refreshToken,
-        });
-        await this.handleResponse(data);
-      })();
+        })
+        await this.handleResponse(data)
+      })()
     }
-    await this.#refresh;
-    this.#refresh = undefined;
+    await this.#refresh
+    this.#refresh = undefined
   }
 }
--- test/tests.ts
+++ test/tests.ts
@@ -1,112 +1,111 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { customAlphabet } from 'nanoid';
+import { customAlphabet } from 'nanoid'
 
-import { Auth, User } from '../src/index.js';
+import { Auth, User } from '../src/index.js'
 
-const apiKey = 'AIzaSyCnFgFqO3d7RbJDcNAp_eO21KSOISCP9IU';
-const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz', 16);
-const domain = '1secmail.com';
+const apiKey = 'AIzaSyCnFgFqO3d7RbJDcNAp_eO21KSOISCP9IU'
+const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz', 16)
+const domain = '1secmail.com'
 
-async function sleep(delayMS: number): Promise<void> {
-  return new Promise((resolve) => {
-    setTimeout(resolve, delayMS);
-  });
+const sleep = async (delayMS: number): Promise<void> =>
+  new Promise(resolve => {
+    setTimeout(resolve, delayMS)
+  })
+
+const decodeHTML = (html: string): string => {
+  const txt = document.createElement('textarea')
+  txt.innerHTML = html
+  return txt.value
 }
 
-function decodeHTML(html: string): string {
-  const txt = document.createElement('textarea');
-  txt.innerHTML = html;
-  return txt.value;
-}
-
-async function secmail(params: { [key: string]: string }): Promise<any> {
-  params.domain = domain;
+const secmail = async (params: { [key: string]: string }): Promise<any> => {
+  params.domain = domain
   const query = Object.keys(params)
-    .map((k) => {
-      return `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`;
+    .map(k => {
+      return `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`
     })
-    .join('&');
-  const url = `https://www.1secmail.com/api/v1/?${query}`;
-  const res = await fetch(url);
-  return res.json();
+    .join('&')
+  const url = `https://www.1secmail.com/api/v1/?${query}`
+  const res = await fetch(url)
+  return res.json()
 }
 
-async function getSigninLink(login: string): Promise<string> {
+const getSigninLink = async (login: string): Promise<string> => {
   const list = (await secmail({
     action: 'getMessages',
     login,
-  })) as any[];
+  })) as any[]
   if (list.length === 0) {
-    await sleep(250);
-    return await getSigninLink(login);
+    await sleep(250)
+    return await getSigninLink(login)
   }
   const message = await secmail({
     action: 'readMessage',
     login,
     id: list[0].id,
-  });
-  const link = /'http.*oobCode.*'/.exec(message.htmlBody)?.[0];
+  })
+  const link = /'http.*oobCode.*'/.exec(message.htmlBody)?.[0]
   if (!link) {
-    throw new Error('link not found in email');
+    throw new Error('link not found in email')
   }
   // decode and trim leading and trailing single quote
-  return decodeHTML(link).slice(1, -1);
+  return decodeHTML(link).slice(1, -1)
 }
 
-QUnit.test('email link signin', async (assert) => {
+QUnit.test('email link signin', async assert => {
   // random new email address
-  const login = nanoid();
-  const email = `${login}@${domain}`;
+  const login = nanoid()
+  const email = `${login}@${domain}`
 
   const states = [
     (u?: User) => assert.equal(u, undefined, 'start off without a user'),
     (u?: User) => assert.equal(u?.email, email, 'sign in'),
     (u?: User) => assert.equal(u, undefined, 'account deleted'),
-  ];
+  ]
 
-  const done = assert.async(states.length);
+  const done = assert.async(states.length)
   const auth = await Auth.new({
     apiKey,
     name: nanoid(),
-  });
+  })
 
   // monitor the various auth state changes
-  let count = 0;
-  let lastUser: User | undefined;
-  auth.subscribe(async (user) => {
-    lastUser = user;
-    const f = states[count];
+  let count = 0
+  let lastUser: User | undefined
+  auth.subscribe(async user => {
+    lastUser = user
+    const f = states[count]
     if (!f) {
-      assert.ok(false, 'unexpected assertion');
+      assert.ok(false, 'unexpected assertion')
     }
-    count++;
-    f(user);
-    done();
-  });
+    count++
+    f(user)
+    done()
+  })
 
   // complete the email sign in flow
-  await auth.sendEmailSigninLink(email);
-  const emailLink = await getSigninLink(login);
-  await auth.handleEmailSigninRedirect(emailLink);
+  await auth.sendEmailSigninLink(email)
+  const emailLink = await getSigninLink(login)
+  await auth.handleEmailSigninRedirect(emailLink)
 
   // force a refresh by mucking with the data
   /* eslint-disable @typescript-eslint/no-non-null-assertion */
-  lastUser!.expiresAt = Date.now() - 10000;
+  lastUser!.expiresAt = Date.now() - 10000
   /* eslint-disable @typescript-eslint/no-non-null-assertion */
-  const oldExpiresAt = lastUser!.expiresAt;
+  const oldExpiresAt = lastUser!.expiresAt
   // @ts-expect-error accessing private members
-  await auth.refresh();
-  assert.notEqual(auth.user?.expiresAt, oldExpiresAt, 'expires changes');
+  await auth.refresh()
+  assert.notEqual(auth.user?.expiresAt, oldExpiresAt, 'expires changes')
 
   // delete the user
-  await auth.delete();
-});
+  await auth.delete()
+})
 
-QUnit.test('email sign-up / sign-in', async (assert) => {
+QUnit.test('email sign-up / sign-in', async assert => {
   // random new email address
-  const login = nanoid();
-  const password = nanoid();
-  const email = `${login}@${domain}`;
+  const login = nanoid()
+  const password = nanoid()
+  const email = `${login}@${domain}`
 
   const states = [
     (u?: User) => assert.equal(u, undefined, 'start off without a user'),
@@ -114,35 +113,35 @@ QUnit.test('email sign-up / sign-in', async (assert) => {
     (u?: User) => assert.equal(u, undefined, 'sign out'),
     (u?: User) => assert.equal(u?.email, email, 'sign in'),
     (u?: User) => assert.equal(u, undefined, 'account deleted'),
-  ];
+  ]
 
-  const done = assert.async(states.length);
+  const done = assert.async(states.length)
   const auth = await Auth.new({
     apiKey,
     name: nanoid(),
-  });
+  })
 
   // monitor the various auth state changes
-  let count = 0;
-  auth.subscribe(async (user) => {
-    const f = states[count];
+  let count = 0
+  auth.subscribe(async user => {
+    const f = states[count]
     if (!f) {
-      assert.ok(false, 'unexpected assertion');
+      assert.ok(false, 'unexpected assertion')
     }
-    count++;
-    f(user);
-    done();
-  });
+    count++
+    f(user)
+    done()
+  })
 
   // complete the email/password flow
   await auth.signUp({
     email,
     password,
-  });
-  await auth.signOut();
+  })
+  await auth.signOut()
   await auth.signIn({
     email,
     password,
-  });
-  await auth.delete();
-});
+  })
+  await auth.delete()
+})
